a09d547e7205e3d20f7cc05304b0dfbb621bd21b
alvarogarcia1994 <alvarogb.barcelona94@gmail.com>
2025-10-31 15:04:41 +0100

    Function to check if a year is leap or not

---
+++ Pruebas/leap.py
@@ -0,0 +1,13 @@
+def isLeap(year):
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
+
+#Main
+def main():
+    #Variable block
+    year = int(input("Year: "))
+    #We validate if the year is leap or not
+    if isLeap(year):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+main()
